56d770d064ae0281293f080b5e364e598ddefbae
JakubKowalczuk <kubi1993@o2.pl>
2019-01-20 14:57:04 +0100

    firstcom

---
+++ src/main/java/pl/jakubkowaczuk/aplikacja/AplikacjaApplication.java
@@ -0,0 +1,14 @@
+package pl.jakubkowaczuk.aplikacja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplikacjaApplication.class, args);
+    }
+
+}
+
